c7b6b99e9b1962c1b37bbb710631ece11e488029
willsbeth <thompsongodswill@gmail.com>
2020-06-04 22:45:06 +0100

    added local settings import

--- island/settings.py
+++ island/settings.py
@@ -135,3 +135,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'island/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
